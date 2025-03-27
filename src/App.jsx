b500c8a90c6bdbf9502f2de05554976e9016060f
Waseem-Baig <waseem20032005@gmail.com>
2025-03-27 13:21:52 +0530

    ALLLL

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,20 @@
-import {  Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import LoadingScreen from "./pages/LoadingScreen";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import AccountInfo from "./pages/AccountInfo"
+import AccountInfo from "./pages/AccountInfo";
 
 function App() {
-   return(
+  return (
     <>
-    
-    
       <Routes>
-         <Route path="/" element={<LoadingScreen />}/>
-         <Route path="/login" element={<Login />} />
-         <Route path="/signup" element={<Signup />} />
-         <Route path="/account" element={<AccountInfo />} />
+        <Route path="/" element={<LoadingScreen />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/account" element={<AccountInfo />} />
       </Routes>
-    
     </>
-   )
-   
-   
+  );
 }
 
 export default App;
